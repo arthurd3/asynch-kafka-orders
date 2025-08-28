a7d44f8123ac5f06097ea96d28bf280371d9ae3c
arthurd3 <arthur_camposl@yahoo.com>
2025-08-28 10:45:09 -0300

    feat: create docker compose

---
+++ async/notification-service/src/test/java/com/arthur/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
